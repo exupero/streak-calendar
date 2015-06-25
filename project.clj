7107363573b6ae45ee50b87f35929c1bcdda4da2
Eric Shull <shull@atomicobject.com>
2015-06-24 23:12:07 -0400

    Create streak calendar.

---
+++ project.clj
@@ -0,0 +1,34 @@
+(defproject streak "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-3195" :exclusions [org.apache.ant/ant]]
+                 [org.clojure/core.match  "0.3.0-alpha4"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [vdom "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
+  :cljsbuild {:builds {:dev {:source-paths ["src"]
+                             :incremental true
+                             :jar true
+                             :assert true
+                             :compiler {:output-to "out/js/streak-dev.js"
+                                        :output-dir "out/js"
+                                        :warnings true
+                                        :elide-asserts true
+                                        :optimizations :none
+                                        :source-map "out/js/streak-dev.js.map"
+                                        :pretty-print true
+                                        :output-wrapper false}}
+                       :prod {:source-paths ["src"]
+                              :incremental true
+                              :jar true
+                              :assert true
+                              :compiler {:output-to "out/js/streak.js"
+                                         :warnings true
+                                         :elide-asserts true
+                                         :externs ["externs/vdom.js" "externs/d3.js"]
+                                         :optimizations :advanced
+                                         :pretty-print false
+                                         :output-wrapper false}}}})
